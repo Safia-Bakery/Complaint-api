8f26a1b079c50cf2cab97803fb912b3e99fcd070
Gayratbek <gayratbekakhmedov@gmail.com>
2024-04-22 13:38:36 +0500

    add comment

--- database.py
+++ database.py
@@ -15,3 +15,5 @@ if SQLALCHEMY_DATABASE_URL is None:
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+#add comment
