34e9f0aca007c85c85c1ccd09b00c5079bf846b4
Gayratbek <gayratbekakhmedov@gmail.com>
2024-08-13 14:59:59 +0500

    add statistics

--- stats/queries/query.py
+++ stats/queries/query.py
@@ -166,7 +166,7 @@ def get_complaint_according_country_expenditure_stats(db: Session, from_date, to
 
                .all())
 
-    stats = {country_name:  str(total_expense)  for country_name, total_expense in service_stats}
+    stats = {country_name:  total_expense  for country_name, total_expense in service_stats}
 
     service_stats = (db.query(request_model.Countries.name, func.sum(request_model.Complaints.expense))
                      .join(request_model.Subcategories,
@@ -180,9 +180,9 @@ def get_complaint_according_country_expenditure_stats(db: Session, from_date, to
 
                      .all())
 
-    quality_stats = {country_name: str(total_expense) for country_name, total_expense in service_stats}
+    quality_stats = {country_name: total_expense for country_name, total_expense in service_stats}
     stats = {
-        "service": service_stats,
+        "service": stats,
         "quality": quality_stats
     }
 
