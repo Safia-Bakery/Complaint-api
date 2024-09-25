5ecea98a24125c7d819fa5260028c18a20813504
Gayratbek <gayratbekakhmedov@gmail.com>
2024-09-25 15:06:32 +0500

    comment migration

--- database.py
+++ database.py
@@ -26,25 +26,25 @@ Base = declarative_base()
 
 
 
-from users.models.user_model import Pages, Actions, Roles, Permissions, Users
-from hrcomplaint.models.hr_model import (Hrspheras,
-                                         HrCategories,
-                                         Hrcomplaints,
-                                         Hrclients,
-                                         Hrcommunications,
-                                         Hrquestions,
-
-                                         )
-from complaints.models.request_model import (
-Countries,
-Categories,
-Subcategories,
-Branchs,
-Complaints,
-Ratings,
-Files,
-Communications,
-
-)
-from complaints.models.helpdesk_clients import HelpDeskClients
-from complaints.models.helpdesk import HelpDeskChats
+# from users.models.user_model import Pages, Actions, Roles, Permissions, Users
+# from hrcomplaint.models.hr_model import (Hrspheras,
+#                                          HrCategories,
+#                                          Hrcomplaints,
+#                                          Hrclients,
+#                                          Hrcommunications,
+#                                          Hrquestions,
+#
+#                                          )
+# from complaints.models.request_model import (
+# Countries,
+# Categories,
+# Subcategories,
+# Branchs,
+# Complaints,
+# Ratings,
+# Files,
+# Communications,
+#
+# )
+# from complaints.models.helpdesk_clients import HelpDeskClients
+# from complaints.models.helpdesk import HelpDeskChats
