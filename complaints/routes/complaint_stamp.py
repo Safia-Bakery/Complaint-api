59f07392916b1f92893260a3856b59b7003e7b81
Gayratbek <gayratbekakhmedov@gmail.com>
2024-10-15 12:29:42 +0500

    add send to telegram button

--- complaints/routes/complaint_stamp.py
+++ complaints/routes/complaint_stamp.py
@@ -8,7 +8,9 @@ from complaints.queries.iikofolders import update_create_folders
 from complaints.utils.utils import file_name_generator
 from services import get_db, get_current_user
 from complaints.utils.api_requests import ApiRoutes
+from complaints.utils.utils import sendtotelegram_inline_buttons
 from users.schemas import user_sch
+from complaints.core.config import BOT_TOKEN_COMPLAINT
 from complaints.schemas.complaint_stampers import CreateComplaintStampers,GetComplaintStampers,DeleteComplaintStampers
 from complaints.queries.complaint_stampers import create_complaint_stampers,delete_complaint_stampers
 
@@ -26,8 +28,21 @@ async def create_complaint_stampers_api(
     """
     Create new complaint
     """
+    query = create_complaint_stampers(db=db, complaint_id=form_data.complaint_id, user_id=form_data.user_id)
 
-    return create_complaint_stampers(db=db, complaint_id=form_data.complaint_id, user_id=form_data.user_id)
+    message_text = f"""id #{form_data.complaint_id}s 
+📁{query.complaint.subcategory.category.name}
+📁{query.complaint.subcategory.name}
+📝{query.complaint.comment}
+📅{query.complaint.created_at.strftime("%d-%m-%Y %H:%M")} 
+👤{query.complaint.client_name}
+📞{query.complaint.client_number}
+🏦{query.complaint.branch.name}
+"""
+    sendtotelegram_inline_buttons(BOT_TOKEN_COMPLAINT, query.user.telegram_id, message_text )
+
+
+    return query
 
 
 @stamp_router.delete("/complaints/stampers/", )
--- complaints/utils/utils.py
+++ complaints/utils/utils.py
@@ -1,6 +1,36 @@
 import random
 import string
+import requests
 
 def file_name_generator(length=20):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
+
+
+
+
+
+def sendtotelegram_inline_buttons(bot_token,chat_id,message_text):
+    keyboard = {
+        'inline_keyboard': [
+            [{'text': 'Yes', 'callback_data': '1'}],
+            [{'text': 'No', 'callback_data': '2'}],
+        ]
+    }
+
+    # Create the request payload
+    payload = {
+        'chat_id': chat_id,
+        'text': message_text,
+        'reply_markup': keyboard,
+        'parse_mode': 'HTML'
+    }
+
+    # Send the request to send the inline keyboard message
+    response = requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", json=payload,)
+
+    # Check the response status
+    if response.status_code == 200:
+        return response
+    else:
+        return False
