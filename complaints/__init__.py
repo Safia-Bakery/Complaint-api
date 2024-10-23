8a0b0e964b7174fe7308d8acefd2b9cf9c03a652
Gayratbek <gayratbekakhmedov@gmail.com>
2024-10-23 10:46:05 +0500

    comment import in database

---
+++ complaints/__init__.py
@@ -0,0 +1,26 @@
+
+from users.models.user_model import Pages, Actions, Roles, Permissions, Users
+from hrcomplaint.models.hr_model import (Hrspheras,
+                                         HrCategories,
+                                         Hrcomplaints,
+                                         Hrclients,
+                                         Hrcommunications,
+                                         Hrquestions,
+
+                                         )
+from complaints.models.request_model import (
+Countries,
+Categories,
+Subcategories,
+Branchs,
+Complaints,
+Ratings,
+Files,
+Communications,
+
+)
+from complaints.models.helpdesk_clients import HelpDeskClients
+from complaints.models.helpdesk import HelpDeskChats
+from complaints.models.iikofolders import IikoFolders
+from complaints.models.iikoproducts import IikoProducts
+from complaints.models.complaint_products import ComplaintProducts
