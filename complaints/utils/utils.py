6cc587c8cacf03678df7e7dee61f6fdb09d45d01
Gayratbek <gayratbekakhmedov@gmail.com>
2024-11-25 10:27:04 +0500

    add password

--- complaints/utils/utils.py
+++ complaints/utils/utils.py
@@ -3,6 +3,15 @@ import string
 import requests
 from datetime import datetime
 
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from complaints.core.config import DOCS_PASSWORD,DOCS_USERNAME
+
+security = HTTPBasic()
+
+
 def file_name_generator(length=20):
     current_date = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
@@ -33,3 +42,16 @@ def sendtotelegram_inline_buttons(bot_token,chat_id,message_text):
         return response
     else:
         return False
+
+
+
+def get_current_user_for_docs(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = DOCS_USERNAME
+    correct_password = DOCS_PASSWORD
+    if credentials.username != correct_username or credentials.password != correct_password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from fastapi.middleware.cors import CORSMiddleware
@@ -22,8 +24,10 @@ from complaints.routes.files import files_router
 from complaints.routes.complaint_stamp import stamp_router
 from users.routes.v2roles import roles_router
 from complaints.routes.clients import client_router
+from complaints.utils.utils import get_current_user_for_docs
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters = {"docExpansion":"none"},docs_url=None, redoc_url=None, openapi_url=None)
+
 
 app.title = "Safia FastApi App"
 app.version = "0.0.1"
@@ -64,6 +68,14 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui(current_user: str = Depends(get_current_user_for_docs)):
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="Custom Swagger UI")
+
+
+@app.get("/openapi.json", include_in_schema=False)
+async def get_open_api_endpoint(current_user: str = Depends(get_current_user_for_docs)):
+    return get_openapi(title="Custom OpenAPI", version="1.0.0", routes=app.routes)
 
 
 @app.get("/", tags=["Home"])
