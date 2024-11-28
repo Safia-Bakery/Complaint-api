aeba948adcf3d13abf6a76ccfb076a85bf802e91
Gayratbek <gayratbekakhmedov@gmail.com>
2024-11-28 14:28:04 +0500

    add new faut

--- bot/Complaintbot/queries/crud.py
+++ bot/Complaintbot/queries/crud.py
@@ -125,6 +125,7 @@ def update_stamper_status(complaint_id,user_id,status):
 def get_one_report(complaint_id):
     db = SessionLocal()
     query = db.query(model.Complaints).filter(model.Complaints.id==complaint_id).first()
+    query.complaint_product = query.complaint_product
     return query
 
 
