9d8aab90ef3550788206fa2485d364e4e94cd806
Gayratbek <gayratbekakhmedov@gmail.com>
2024-11-29 11:03:24 +0500

    edit image

--- complaints/utils/generate_stamp.py
+++ complaints/utils/generate_stamp.py
@@ -64,9 +64,9 @@ def generate_stamp(report):
 
     quality_path = '/var/www/Complaint-api/assets/controlquality-Photoroom.png'
     image_x = 285  # X position of the image
-    image_y = 740  # Y position of the image
-    image_width = 70  # Desired width of the image
-    image_height = 40  # Desired height of the image
+    image_y = 750  # Y position of the image
+    image_width = 90  # Desired width of the image
+    image_height = 60  # Desired height of the image
     pdf.drawImage(quality_path, image_x, image_y, width=image_width, height=image_height)
 
     # Example: Place the title at a specific position (centered)
