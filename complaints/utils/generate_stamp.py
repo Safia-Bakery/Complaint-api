001aa5760e7202bf10fb758752d043c91fee66f1
Gayratbek <gayratbekakhmedov@gmail.com>
2024-12-26 14:26:44 +0500

    add new feature

--- complaints/utils/generate_stamp.py
+++ complaints/utils/generate_stamp.py
@@ -71,7 +71,7 @@ def generate_stamp(report):
 
     # Example: Place the title at a specific position (centered)
 
-    report_comment= add_newline_every_n_chars(report.comment,n=40)
+    report_comment= add_newline_every_n_chars(report.comment,n=50)
 
     # Data for the table
     data = [
@@ -97,7 +97,7 @@ def generate_stamp(report):
 
     # Define table position (x, y) - here starting at 200 points from bottom-left corner
     table.wrapOn(pdf, 400, 600)   # Width and height to wrap content (table size)
-    table.drawOn(pdf, 50, 620)    # Position (x=50, y=600) to place the table
+    table.drawOn(pdf, 50, 600)    # Position (x=50, y=600) to place the table
 
     # Add a paragraph of text below the table
     pdf.setFont("DejaVu", 12)
