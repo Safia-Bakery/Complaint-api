c35d2bfb863ef8164b2e429d3fb83633e82d658e
Gayratbek <gayratbekakhmedov@gmail.com>
2025-02-18 15:25:27 +0500

    add new feate

--- complaints/__init__.py
+++ complaints/__init__.py
@@ -1,13 +1,13 @@
-
-from users.models.user_model import Pages, Actions, Roles, Permissions, Users
-from hrcomplaint.models.hr_model import (Hrspheras,
-                                         HrCategories,
-                                         Hrcomplaints,
-                                         Hrclients,
-                                         Hrcommunications,
-                                         Hrquestions,
-
-                                         )
+#
+# from users.models.user_model import Pages, Actions, Roles, Permissions, Users
+# from hrcomplaint.models.hr_model import (Hrspheras,
+#                                          HrCategories,
+#                                          Hrcomplaints,
+#                                          Hrclients,
+#                                          Hrcommunications,
+#                                          Hrquestions,
+#
+#                                          )
 from complaints.models.request_model import (
 Countries,
 Categories,
