ba90851689b1ac8c44fd92de9e10a1c3c44f0c60
Gayratbek <gayratbekakhmedov@gmail.com>
2025-02-20 17:49:10 +0500

    edit send file

--- bot/Complaintbot/service.py
+++ bot/Complaintbot/service.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import requests
+import mimetypes
+
 def transform_list(lst, size, key):
     # if key=='id':
     
@@ -28,21 +30,45 @@ def validate_only_date(date_string):
         return False
 
 
+# def send_file_telegram(bot_token, chat_id, file_path, caption=None, reply_to_message_id=None):
+#     url = f"https://api.telegram.org/bot{bot_token}/sendDocument"
+#
+#     # 'files' for sending documents is a dictionary with a tuple (optional filename, file data)
+#     if file_path is None:
+#         return requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", data={"chat_id": chat_id, "text": caption}).json()
+#     else:
+#
+#         with open(file_path, 'rb') as file:
+#             files = {'document': (file_path, file)}
+#             data = {'chat_id': chat_id,'reply_to_message_id':reply_to_message_id}
+#
+#             # Make a POST request to the Telegram API
+#             response = requests.post(url, data=data, files=files)
+#         return response.json()
+
+
 def send_file_telegram(bot_token, chat_id, file_path, caption=None, reply_to_message_id=None):
-    url = f"https://api.telegram.org/bot{bot_token}/sendDocument"
-
-    # 'files' for sending documents is a dictionary with a tuple (optional filename, file data)
     if file_path is None:
-        return requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", data={"chat_id": chat_id, "text": caption}).json()
-    else:
+        return requests.post(
+            f"https://api.telegram.org/bot{bot_token}/sendMessage",
+            data={"chat_id": chat_id, "text": caption}
+        ).json()
 
-        with open(file_path, 'rb') as file:
-            files = {'document': (file_path, file)}
-            data = {'chat_id': chat_id,'reply_to_message_id':reply_to_message_id}
+    # Detect file type
+    mime_type, _ = mimetypes.guess_type(file_path)
+    is_image = mime_type and mime_type.startswith("image")
 
-            # Make a POST request to the Telegram API
-            response = requests.post(url, data=data, files=files)
-        return response.json()
+    url = f"https://api.telegram.org/bot{bot_token}/sendPhoto" if is_image else f"https://api.telegram.org/bot{bot_token}/sendDocument"
+    file_key = "photo" if is_image else "document"
+
+    with open(file_path, 'rb') as file:
+        files = {file_key: (file_path, file)}
+        data = {'chat_id': chat_id, 'caption': caption, 'reply_to_message_id': reply_to_message_id}
+
+        # Make a POST request to the Telegram API
+        response = requests.post(url, data=data, files=files)
+
+    return response.json()
 
 
     
