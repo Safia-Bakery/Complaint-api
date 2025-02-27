d46a060b5cc9846c5ae20bc05b39544ef15812cc
Bakhtiyor Bakhriddinov <dataeng27@gmail.com>
2025-02-27 15:28:02 +0500

    Fixing bug

--- hrcomplaint/models/hr_model.py
+++ hrcomplaint/models/hr_model.py
@@ -1,27 +1,17 @@
+import pytz
 from sqlalchemy import (
     Column,
     Integer,
     String,
     ForeignKey,
-    Float,
     DateTime,
-    Boolean,
     BIGINT,
-    Table,
-    Time,
-    JSON,
-    VARCHAR,
-    Date,
 )
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from sqlalchemy.dialects.postgresql import UUID, ARRAY, JSONB
-from datetime import datetime
-import pytz
-import uuid
 
-from database import SessionLocal, Base
+from database import Base
 
 timezonetash = pytz.timezone("Asia/Tashkent")
 
